82072e136382fde7dea954199a10ed839a57b069
giovanna fernandes de oliveira <162985652+giool3irah@users.noreply.github.com>
2024-04-24 08:21:00 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,24 @@
+ listaNomes = []
+
+
+ listaNomes.append("Edson")
+ listaNomes.append("Giovanna")
+ listaNomes.append("Sofia")
+
+ print(listaNomes)
+ print(listaNomes[1])
+
+ for nome in listaNomes:
+     print(nome)
+     #DICIONÁRIOS
+primeiroDicionário = {}
+
+primeiroDicionário['nome'] = "Edson"
+primeiroDicionário['tipo'] = 'Professor'
+primeiroDicionário['número'] = 9
+print(primeiroDicionário)
+print(primeiroDicionário["tipo"])
+
+#Anatomia 
+# lista[índice] = valor
+# dicionário[chave] = valor
